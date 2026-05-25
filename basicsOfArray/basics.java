90a758246b0fc02dcfe67319129dd80825ae6089
siddhantKumar07 <siddhantk80044@gmail.com>
2026-05-26 00:23:48 +0530

    creting the array

--- basicsOfArray/basics.java
+++ basicsOfArray/basics.java
@@ -1,20 +1,19 @@
-package basicsOfArray;
+// package basicsOfArray;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class basics {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-     int arr[] = new int[5];
-     for (int i = 0; i < arr.length; i++) {
-        arr[i] =sc.nextInt();
-     
-     }
-
-     System.out.println(Arrays.toString(arr));
-
+        Scanner sc = new Scanner(System.in);
+   System.out.println("enter the elements of array");
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    for (int i : arr) {
+        System.out.println(i);
+    }
+        System.out.println(Arrays.toString(arr));
     }
 }
