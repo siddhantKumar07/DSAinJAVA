adb7c1e80571105903d634fe62ba15ebbe9cd9f9
siddhantKumar07 <siddhantk80044@gmail.com>
2026-06-18 12:29:36 +0530

    doing qus of array : finding avg

---
+++ basicsOfArray/average.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float avg;
+        int sum=0;
+    int arr[]=new int[5];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i]=sc.nextInt();
+    }
+     for (int i = 0; i < arr.length; i++) {
+        sum+=arr[i];
+     }
+     avg=sum/arr.length;
+     System.out.println("the average is : "+avg);
+    }
+}
