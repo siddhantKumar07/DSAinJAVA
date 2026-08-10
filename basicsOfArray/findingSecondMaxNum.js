1d1f7b3bfc02e9937fc9ba2ace38c07abe96f133
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-10 23:02:45 +0530

    adding some logics

--- basicsOfArray/findingSecondMaxNum.js
+++ basicsOfArray/findingSecondMaxNum.js
@@ -1,19 +1,22 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr=[1,2,3,4,5,6,7,8,9,10]
 
-function secondLargest(array){
-let largest=0;
-let secondLargest=0;
-for(const elem of array){
-    if(elem>=largest){
-        largest = elem;
+function secondLargest(arr){
+let largest=-Infinity
+let secondLargest=-Infinity
+
+for(let elem of arr){
+if(elem>largest){
+    largest=elem
+    secondLargest=largest
+
+    if(elem>secondLargest){
+        secondLargest= elem
+
     }
 }
-for(const elem of array){
-    if(elem>=secondLargest&&secondLargest<largest){
-        secondLargest=elem;
-    }
 }
 return secondLargest
 }
-let reruned = secondLargest(array)
-console.log(reruned)
+
+let secondLargestElem = secondLargest(arr)
+console.log(secondLargestElem)
