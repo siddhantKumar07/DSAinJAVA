eba7f0e904f42539ed2f742fe6d749206287cdc2
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-15 23:34:48 +0530

    checking palindrome

--- basicsOfArray/checkPalindrome.js
+++ basicsOfArray/checkPalindrome.js
@@ -1,5 +1,18 @@
 const checkpalindrome=(value)=>{
     if(typeof(value)=="string"){
+        const reverse =value.split("").reverse().join("")
+        if(value==reverse){
+            console.log("its a palindrome number")
+        }
+        else{
+            console.log("its not a palindrome number")
+        }
+
     }
+    if(typeof(value)=="number"){
+
+
+    }
+  
 }
-checkpalindrome("555")
+checkpalindrome("madam")
