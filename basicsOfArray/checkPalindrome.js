9686273b0c06ab6e5b291269628d2db2c2ad0c03
siddhantKumar07 <siddhantk80044@gmail.com>
2026-08-15 23:38:38 +0530

    checking for both number and string

--- basicsOfArray/checkPalindrome.js
+++ basicsOfArray/checkPalindrome.js
@@ -10,7 +10,15 @@ const checkpalindrome=(value)=>{
 
     }
     if(typeof(value)=="number"){
-
+   const reverse=0,temp;
+   temp=value;
+   while(temp>0){
+     reverse =temp/10+reverse*10;
+     temp=Math.trunc(temp/10)
+   }
+   if(reverse==value){
+    console.log("its a palindrome")
+   }else console.log("its not a palindrom")
 
     }
   
