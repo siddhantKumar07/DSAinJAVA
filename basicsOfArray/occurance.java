ba114d396a50664cfbca75e031b68c177b407a78
siddhantKumar07 <siddhantk80044@gmail.com>
2026-09-21 21:45:23 +0530

    solving dsa

--- basicsOfArray/occurance.java
+++ basicsOfArray/occurance.java
@@ -6,7 +6,10 @@ public class occurance {
         for (int i = 0; i < arr.length; i++) {
            arr2[arr[i]]=arr2[arr[i]]+1;
         }
-        System.out.println(Arrays.toString(arr2));
+      for (int i = 0; i < arr2.length; i++) {
+        if(arr2[i]>0)
+        System.out.println("Element "+i+" occurs "+arr2[i]+" times");
+      }
     }
     public static void main(String[] args) {
         int arr[]={1,2,2,3,3,3,3,3,3,3,3,3,4,5,4,6};
